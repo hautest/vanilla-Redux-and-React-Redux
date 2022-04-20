8d29729ec7118becdc5753317b1064c37a557972
lee <lee@sayongjaui-MacBookAir.local>
2022-04-20 20:40:36 +0900

    redux-study-banilla-js-redux-yes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
+import { createStore } from "redux";
+
 const add = document.querySelector(".add");
 const minus = document.querySelector(".minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
+const countStore = createStore(countModifier);
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  countStore.dispatch({ type: ADD });
 };
+
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
