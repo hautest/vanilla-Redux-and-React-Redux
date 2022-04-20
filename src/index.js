734bcfa6311f05086d8a44564e8ccc1920ee7291
lee <lee@sayongjaui-MacBookAir.local>
2022-04-20 18:07:34 +0900

    redux-study-banilla-js-redux-no

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const add = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const number = document.querySelector("span");
+
+let count = 0;
+//ddddddddㅇㅇ
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count = count + 1;
+  updateText();
+};
+const handleMinus = () => {
+  count = count - 1;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
